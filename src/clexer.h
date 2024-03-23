42c5e84d3d956261e5855a5292f9b0aa30fc9c52
malloc-nbytes <zacharydhaskins@yahoo.com>
2024-03-23 16:19:40 -0500

    update

--- src/clexer.h
+++ src/clexer.h
@@ -4,6 +4,7 @@
 #ifdef CLEXER_IMPL
 
 #include <assert.h>
+#include <ctype.h>
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -45,25 +46,47 @@ Arena *arena_create(size_t cap)
 
 // ********** HELPERS ********** //
 
-char *file_to_str(const char *filepath)
+size_t consume_until(char *s, int (*predicate)(char))
 {
-  FILE *fp = fopen(filepath, "rb");
+  size_t i;
+  for (i = 0; s[i] && !predicate(s[i]); ++i);
+  return i;
+}
 
-  char *buffer = 0;
-  long len;
+int is_quote(char c)
+{
+  return c == '"';
+}
 
-  if (fp) {
-    fseek(fp, 0, SEEK_END);
-    len = ftell(fp);
-    fseek(fp, 0, SEEK_SET);
-    buffer = malloc (len);
+int nisdigit(char c)
+{
+  return !isdigit(c);
+}
 
-    if (buffer) {
-      fread(buffer, 1, len, fp);
-    }
+// Code from:
+//   chux - Reinstate Monica
+//   https://stackoverflow.com/questions/174531/how-to-read-the-content-of-a-file-to-a-string-in-c
+char *file_to_str(char *filepath) {
+  FILE *f = fopen(filepath, "rb");
 
-    fclose(fp);
+  if (f == NULL || fseek(f, 0, SEEK_END)) {
+    return NULL;
   }
+
+  long length = ftell(f);
+  rewind(f);
+  if (length == -1 || (unsigned long) length >= SIZE_MAX) {
+    return NULL;
+  }
+
+  size_t ulength = (size_t)length;
+  char *buffer = malloc(ulength + 1);
+
+  if (buffer == NULL || fread(buffer, 1, ulength, f) != ulength) {
+    free(buffer);
+    return NULL;
+  }
+  buffer[ulength] = '\0';
 
   return buffer;
 }
@@ -83,6 +106,7 @@ typedef struct Token {
   size_t row;
   size_t col;
   char *fp;
+  struct Token *next;
 } Token;
 
 typedef struct Lexer {
@@ -91,6 +115,27 @@ typedef struct Lexer {
   size_t len;
   Arena *arena;
 } Lexer;
+
+char *tokentype_to_str(TokenType type)
+{
+  switch (type) {
+  case TOKENTYPE_EOF:
+    return "EOF";
+  case TOKENTYPE_INTLIT:
+    return "INTLIT";
+  case TOKENTYPE_STRLIT:
+    return "STRLIT";
+  case TOKENTYPE_IDENT:
+    return "IDENT";
+  case TOKENTYPE_LPAREN:
+    return "LPAREN";
+  case TOKENTYPE_RPAREN:
+    return "RPAREN";
+  default:
+    assert(0 && "unknown tokentype");
+  }
+  return NULL;
+}
 
 Token *token_alloc(Lexer *lexer,
                    char *start, size_t len,
@@ -101,19 +146,53 @@ Token *token_alloc(Lexer *lexer,
 
   char *lexeme = (char *)arena_alloc(lexer->arena, len+1);
   (void)strncpy(lexeme, start, len);
-  lexeme[len+1] = '\0';
+  lexeme[len] = '\0';
 
   tok->lexeme = lexeme;
   tok->type = type;
   tok->row = row;
   tok->col = col;
   tok->fp = fp;
+  tok->next = NULL;
 
   return tok;
 }
 
-Lexer lex_file(char *filepath)
+void lexer_append(Lexer *lexer, Token *tok)
 {
+  if (!lexer->hd) {
+    lexer->hd = tok;
+    lexer->tl = tok;
+  } else {
+    lexer->tl->next = tok;
+    lexer->tl = tok;
+  }
+  ++lexer->len;
+}
+
+Token *lexer_next(Lexer *lexer)
+{
+  Token *tok = lexer->hd;
+  if (tok) {
+    lexer->hd = tok->next;
+    lexer->len--;
+  }
+  return tok;
+}
+
+void lexer_dump(Lexer *lexer)
+{
+  Token *tok;
+  while ((tok = lexer_next(lexer))) {
+    printf("lexeme: %s, type: %s, row: %zu, col: %zu, fp: %s\n",
+           tok->lexeme, tokentype_to_str(tok->type), tok->row, tok->col, tok->fp);
+  }
+}
+
+Lexer lex_file(char *filepath, char **keywords)
+{
+  (void)keywords;
+
   char *src = file_to_str(filepath);
   Lexer lexer = (Lexer) {
     .hd = NULL,
@@ -122,9 +201,11 @@ Lexer lex_file(char *filepath)
     .arena = arena_create(32768),
   };
 
-  for (size_t i = 0, row = 1, col = 1; src[i]; ++i) {
+  size_t i, row, col;
+  for (i = 0, row = 1, col = 1; src[i]; ++i) {
     char c = src[i];
-    char *lexeme = src+i;
+    Token *tok = NULL;
+
     switch (c) {
     case '\r':
     case '\n':
@@ -135,22 +216,58 @@ Lexer lex_file(char *filepath)
     case ' ':
       ++col;
       break;
-    case '(':
-      break;
-    case '"':
-      assert(0 && "unimplemented");
-      break;
+    case '(': {
+      tok = token_alloc(&lexer, src+i, 1, TOKENTYPE_LPAREN, row, col, filepath);
+      lexer_append(&lexer, tok);
+      ++col;
+    } break;
+    case ')': {
+      tok = token_alloc(&lexer, src+i, 1, TOKENTYPE_RPAREN, row, col, filepath);
+      lexer_append(&lexer, tok);
+      ++col;
+    } break;
+    case '"': {
+      size_t strlit_len = consume_until(src+i+1, is_quote);
+      tok = token_alloc(&lexer, src+i+1, strlit_len, TOKENTYPE_STRLIT, row, col, filepath);
+      lexer_append(&lexer, tok);
+      i += 1+strlit_len;
+      col += 1+strlit_len+1;
+    } break;
     case '\'':
       assert(0 && "unimplemented");
       break;
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9': {
+      size_t intlit_len = consume_until(src+i, nisdigit);
+      tok = token_alloc(&lexer, src+i, intlit_len, TOKENTYPE_INTLIT, row, col, filepath);
+      lexer_append(&lexer, tok);
+      i += intlit_len-1;
+      col += intlit_len;
+    } break;
     default:
       assert(0 && "unimplemented");
       break;
     }
   }
 
+  lexer_append(&lexer, token_alloc(&lexer, "EOF", 3, TOKENTYPE_EOF, row, col, filepath));
+
   free(src);
   return lexer;
+}
+
+void lexer_free(Lexer *lexer)
+{
+  arena_free(lexer->arena);
+  lexer->len = 0;
 }
 
 #endif // CLEXER_IMPL
